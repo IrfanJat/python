4e76700433116ca5574d27d86824c9391d180097
Irfan ALi <jatirfan110@gmail.com>
2025-10-26 17:19:40 +0500

    Updated

---
+++ lists.py
@@ -0,0 +1,29 @@
+#lists in python
+
+user_list = ['Irfan','Ali','Affan']
+print(user_list) 
+
+print(user_list[0])
+print(user_list[1])
+print(user_list[2])
+#Adding item to the end of list
+user_list.append('Tony')
+print(user_list)
+
+#Removing item from the list
+user_list.remove('Tony')
+print(user_list)
+
+#No of items in a list
+
+print (len(user_list)) 
+
+#sorting of list 
+
+user_list.sort()
+print(user_list)
+
+#Reverse the list
+
+user_list.reverse()
+print(user_list)
